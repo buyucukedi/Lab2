22b0a75131683615566551bca3cd7200cb828be0
buyucukedi <scccgdh@gmail.com>
2024-10-07 10:54:32 +0200

    Changes in Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter 2 variables:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         Adder adder = new Adder();
-        System.out.println(adder.add(1,2));
+        System.out.println(adder.add(a,b));
         Substractor substractor = new Substractor();
-        System.out.println(substractor.substract(6,3));
+        System.out.println(substractor.substract(a,b));
 
     }
 }
